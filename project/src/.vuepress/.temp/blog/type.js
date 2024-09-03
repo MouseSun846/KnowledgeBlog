add4e174a2ae0655f028ae4918f54c07db259b4f
mousesun <3026098675@qq.com>
2024-09-03 11:59:46 +0800

    update

--- project/src/.vuepress/.temp/blog/type.js
+++ project/src/.vuepress/.temp/blog/type.js
@@ -1,3 +1,3 @@
-      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[2,6,7,10,8,9,0,11,1,3,12,4,5]}},"star":{"/":{"path":"/star/","indexes":[]}},"timeline":{"/":{"path":"/timeline/","indexes":[2,6,7,10,8,9,0,11,1,3,12,4,5]}}};
+      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[2,6,8,11,9,10,0,12,1,3,7,4,5]}},"star":{"/":{"path":"/star/","indexes":[]}},"timeline":{"/":{"path":"/timeline/","indexes":[2,6,8,11,9,10,0,12,1,3,7,4,5]}}};
       
       
--- project/src/.vuepress/.temp/pages/技术科普/工具.html.vue
+++ project/src/.vuepress/.temp/pages/技术科普/工具.html.vue
@@ -2,8 +2,7 @@
 <h3 id="ping-安装" tabindex="-1"><a class="header-anchor" href="#ping-安装"><span>ping 安装</span></a></h3>
 <div class="language- line-numbers-mode" data-highlighter="shiki" data-ext="" data-title="" style="--shiki-light:#24292e;--shiki-dark:#abb2bf;--shiki-light-bg:#fff;--shiki-dark-bg:#282c34"><pre v-pre class="shiki shiki-themes github-light one-dark-pro vp-code"><code><span class="line"><span>apt install iputils-ping</span></span></code></pre>
 <div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><h3 id="获取公网ip" tabindex="-1"><a class="header-anchor" href="#获取公网ip"><span>获取公网ip</span></a></h3>
-<div class="language- line-numbers-mode" data-highlighter="shiki" data-ext="" data-title="" style="--shiki-light:#24292e;--shiki-dark:#abb2bf;--shiki-light-bg:#fff;--shiki-dark-bg:#282c34"><pre v-pre class="shiki shiki-themes github-light one-dark-pro vp-code"><code><span class="line"><span>curl ip.sb</span></span>
-<span class="line"><span>``</span></span></code></pre>
-<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div></div></div></div></template>
+<div class="language- line-numbers-mode" data-highlighter="shiki" data-ext="" data-title="" style="--shiki-light:#24292e;--shiki-dark:#abb2bf;--shiki-light-bg:#fff;--shiki-dark-bg:#282c34"><pre v-pre class="shiki shiki-themes github-light one-dark-pro vp-code"><code><span class="line"><span>curl ip.sb</span></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div></div></template>
 
 
