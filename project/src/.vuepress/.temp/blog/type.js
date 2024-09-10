aeabbae80e4bdbf301b4fce747b8d5f2ae871afc
mousesun <3026098675@qq.com>
2024-09-10 12:43:53 +0800

    update

--- project/src/.vuepress/.temp/blog/type.js
+++ project/src/.vuepress/.temp/blog/type.js
@@ -1,3 +1,3 @@
-      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[9,2,5,8,12,10,11,0,13,1,3,7,4,6]}},"star":{"/":{"path":"/star/","indexes":[]}},"timeline":{"/":{"path":"/timeline/","indexes":[9,2,5,8,12,10,11,0,13,1,3,7,4,6]}}};
+      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[9,2,6,8,12,10,11,0,13,1,3,7,4,5]}},"star":{"/":{"path":"/star/","indexes":[]}},"timeline":{"/":{"path":"/timeline/","indexes":[9,2,6,8,12,10,11,0,13,1,3,7,4,5]}}};
       
       
