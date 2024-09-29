5b636712a95510946da9029e47e0dbc0b3fc3fcc
mousesun <3026098675@qq.com>
2024-09-29 11:40:09 +0800

    update

--- project/src/.vuepress/.temp/blog/type.js
+++ project/src/.vuepress/.temp/blog/type.js
@@ -1,3 +1,3 @@
-      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[7,10,18,17,16,9,2,6,8,13,11,12,0,14,1,3,15,4,5]}},"star":{"/":{"path":"/star/","indexes":[]}},"timeline":{"/":{"path":"/timeline/","indexes":[7,10,18,17,16,9,2,6,8,13,11,12,0,14,1,3,15,4,5]}}};
+      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[5,10,18,17,16,9,2,7,8,13,11,12,0,14,1,3,15,4,6]}},"star":{"/":{"path":"/star/","indexes":[]}},"timeline":{"/":{"path":"/timeline/","indexes":[5,10,18,17,16,9,2,7,8,13,11,12,0,14,1,3,15,4,6]}}};
       
       
--- project/src/.vuepress/.temp/pages/异构计算/异构计算.html.vue
+++ project/src/.vuepress/.temp/pages/异构计算/异构计算.html.vue
@@ -3,6 +3,15 @@
 </div>
 <img src="/assets/images/异构计算.jpg" width="400" style="margin: auto auto auto auto; display: block;" />
 <p>注： 若二维码失效，加我V: KomorebiTimothy 备注：异构计算交流</p>
+<h2 id="参考资料" tabindex="-1"><a class="header-anchor" href="#参考资料"><span>参考资料</span></a></h2>
+<p><a href="https://www.bilibili.com/video/BV1fRt2efEZC/?spm_id_from=333.337.search-card.all.click&amp;vd_source=f8b6af2d2ca3f4e95494da62546a0fb9" target="_blank" rel="noopener noreferrer">异构万卡集群，GPU与国产计算卡芯片异构通信</a></p>
+<p><a href="https://mp.weixin.qq.com/s/3c15wxlU6jfr-DfqY9e-gw" target="_blank" rel="noopener noreferrer">论文 异构GPU集群上大模型训练推理续</a></p>
+<p><a href="https://mp.weixin.qq.com/s/bO0Hn1GhoMM5uwT9I-3H-g" target="_blank" rel="noopener noreferrer">论文 异构GPU集群上大模型训练推理续2</a></p>
+<p><a href="https://mp.weixin.qq.com/s/0syMz6gpYs3F9tXJ4q0uyQ" target="_blank" rel="noopener noreferrer">论文 MLaaS：阿里大规模异构GPU集群中任务分析和调度</a></p>
+<p><a href="https://mp.weixin.qq.com/s/KrtrWTMBaAnXECK8dgMX7g" target="_blank" rel="noopener noreferrer">ATC2024 异构GPU集群上大模型训练推理</a></p>
+<p><a href="https://mp.weixin.qq.com/s/yNLWtE9GmePdZrmmmGdZnA" target="_blank" rel="noopener noreferrer">PATHWAYS：谷歌大规模异构计算编排调度系统（Jeff Dean 和 Sanjay Ghemawat联合出品）</a></p>
+<p><a href="https://zhuanlan.zhihu.com/p/656973882" target="_blank" rel="noopener noreferrer">Pathways: Asynchronous Distributed Dataflow for ML</a></p>
+<p><a href="https://mp.weixin.qq.com/s/2zCTeFht_YXh1u7kRiIhFA" target="_blank" rel="noopener noreferrer">VLDB23：Pytorch FSDP 分布式训练新篇章</a></p>
 </div></template>
 
 
