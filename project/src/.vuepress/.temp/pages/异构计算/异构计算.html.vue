be4b279a6353c9c297b3fd5a68bc4fc1c716d34f
mousesun <3026098675@qq.com>
2024-09-29 11:29:33 +0800

    update

---
+++ project/src/.vuepress/.temp/pages/异构计算/异构计算.html.vue
@@ -0,0 +1,8 @@
+<template><div><div class="hint-container tip">
+<p class="hint-container-title">异构计算交流</p>
+</div>
+<img src="/assets/images/异构计算.jpg" width="400" style="margin: auto auto auto auto; display: block;" />
+<p>注： 若二维码失效，加我V: KomorebiTimothy 备注：异构计算交流</p>
+</div></template>
+
+
--- project/src/.vuepress/sidebar.ts
+++ project/src/.vuepress/sidebar.ts
@@ -1,3 +1,4 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export default sidebar({"/介绍/":"structure", "/云原生/":"structure", "/杂谈/":"structure","/技术科普/":"structure"});
+export default sidebar({"/介绍/":"structure", "/云原生/":"structure", "/杂谈/":"structure","/技术科普/":"structure",
+"/异构计算/":"structure"});
